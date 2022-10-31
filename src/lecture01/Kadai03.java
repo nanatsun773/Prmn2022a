21c728456aa62e0a3d0b4725670464065c111f7e
nanatsun773 <b2201580@photon.chitose.ac.jp>
2022-10-31 23:42:37 +0900

    課題1-3

--- src/lecture01/Kadai03.java
+++ src/lecture01/Kadai03.java
@@ -1,4 +1,14 @@
 package lecture01;
 
 public class Kadai03 {
+
+    public static void main(String[] args) {
+        int age = 20;
+
+        if(age < 20) {
+            System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        }else {
+            System.out.println("I am " + age + " years old so I drink liquor.");
+        }
+    }
 }
