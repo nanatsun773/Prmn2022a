6f6240c88dbd8d443b7d9e3a5623cb9cbe739d79
nanatsun773 <b2201580@photon.chitose.ac.jp>
2022-10-31 16:36:03 +0900

    課題1-1

---
+++ src/lecture01/Kadai01.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class Kadai01 {
+
+    public static void main(String[] args) {
+        System.out.println("B2201580 Nanami Tanimura");
+    }
+}
---
+++ src/lecture01/Main.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
