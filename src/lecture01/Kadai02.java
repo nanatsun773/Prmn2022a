e22ad0b51f304e47c4fb85ab8025c9c28911dc69
nanatsun773 <b2201580@photon.chitose.ac.jp>
2022-10-31 23:35:54 +0900

    課題1-2

---
+++ src/lecture01/Kadai02.java
@@ -0,0 +1,9 @@
+package lecture01;
+
+public class Kadai02 {
+
+    public static void main(String[] args) {
+        int studentNumber = 2201580;
+        System.out.println("B" + studentNumber + " Nanami " + "Tanimura");
+    }
+}
