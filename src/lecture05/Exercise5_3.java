9f2e930f83ddfa8d2d5898f8a694ae0cb43fab79
nanatsun773 <b2201580@photon.chitose.ac.jp>
2022-11-11 23:33:09 +0900

    lecture5

---
+++ src/lecture05/Exercise5_3.java
@@ -0,0 +1,9 @@
+package lecture05;
+
+public class Exercise5_3 {
+
+    public static void main(String[] args) {
+        Insect insect = new Butterfly();
+        insect.move();
+    }
+}
---
+++ src/lecture05/Exercise5_5.java
@@ -0,0 +1,9 @@
+package lecture05;
+
+public class Exercise5_5 {
+
+    public static void main(String[] args) {
+        Insect insect = new Locust();
+        insect.move();
+    }
+}
